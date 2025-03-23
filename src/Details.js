23aa204284bef47e23620882ca7eaebff5010bf5
Anchal Mishra <gautamanchal143f@gmail.com>
2025-03-23 19:04:40 +0530

    Project added

---
+++ src/Details.js
@@ -0,0 +1,149 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/logo.svg";
+import logo from "./assets/logo2.svg";
+// Profile Image
+import profile from "./assets/profile.jpg";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+// Porject Images
+import projectImage1 from "./assets/projects/Yummy.png";
+import projectImage2 from "./assets/projects/E-commerce.png";
+
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Anchal Mishra",
+  tagline: "A React Js Developer and Web Developer",
+  img: profile,
+  about: `I am a dedicated React.js Developer working full-time, with 6+ months of experience in building modern, 
+  responsive web applications. Graduate in  B.Tech (CSE) at JNCT Rewa, I specialize in front-end development 
+  using React.js, Bootstrap, Tailwind , REST API integration and  manage state using tools like Redux or Context API.
+
+With a strong foundation in the MERN stack, I focus on delivering high-performance, user-friendly interfaces that enhance the overall digital experience. Passionate about problem-solving and modern web technologies, I continuously strive to create scalable and efficient applications.`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/anchalmishra13/",
+  github: "https://github.com/anchal1233",
+  
+  instagram: "https://www.instagram.com/",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "React Developer - Frontend Engineer (UI/UX Expertise) ",
+    Company: `Crebaco Global Pvt Ltd`,
+    Location: "Ahemdabad,Gujrat",
+    Type: "Full Time",
+    Duration: "Jan 2025 - Present",
+  },
+  {
+    Position: "Trainee MERN stack Developer",
+    Company: `Ypsilon It Solution Pvt Ltd`,
+    Location: "Indore",
+    Type: "Internship",
+    Duration: "May 2024 - Nov 2024",
+  },
+  {
+    Position: "Node Js Intern",
+    Company: `Algosoft Apps Technologies`,
+    Location: "Noida(Remote)",
+    Type: "Internship",
+    Duration: "June 2023 - July 2023",
+  },
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "BTech In Computer Science",
+    Company: "Jawaharlal Nehru College Of Technology",
+    Location: "Rewa",
+    Type: "Grade : 8.77",
+    Duration: "Nov 2020 - June 2024",
+  },
+  {
+    Position: "12th",
+    Company: `Saraswati Shishu Mandir Higher Secondary School`,
+    Location: "Mauganj",
+    Type: "Percentage :- 72%",
+    Duration: "2020",
+  },
+  {
+    Position: "10th",
+    Company: `Saraswati Shishu Mandir Higher Secondary School`,
+    Location: "Mauganj",
+    Type: "Percentage :- 90%",
+    Duration: "2018",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "CHAI Lovers",
+    image: projectImage1,
+    description: `Chai Lovers is a dynamic and interactive website designed for chai lovers, 
+    inspired by popular chai cafés like Chai Sutta Bar. 
+    The platform allows users to explore different chai flavors, place online orders, 
+    and engage with a community of tea enthusiasts.`,
+    techstack: "HTML/CSS, JavaScript,PHP",
+    previewLink: "https://yummy-phi-umber.vercel.app",
+    githubLink: "https://github.com/anchal1233/Yummy",
+  },
+  {
+    title: "E-Commerce Website",
+    image: projectImage2,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "React js, Node js, Express Js, MOngoDB",
+    previewLink: "https://e-commerce-six-theta-17.vercel.app/",
+    githubLink: "https://github.com/anchal1233/E-Commerce",
+  },
+ 
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "gautamanchal143f@gmail.com",
+  phone: "+91 8770270358",
+};
